eeb68125325efe010ced062e10fd93285d4f6336
ksagit <kuba.kielaszek@gmail.com>
2025-05-13 02:38:38 +0200

    Player movement and grouping

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ from player import Player
 
 def main():
     pygame.init()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock() 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
@@ -15,8 +20,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
